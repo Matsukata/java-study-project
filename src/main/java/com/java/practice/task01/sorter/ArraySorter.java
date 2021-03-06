c9ab0080ff8f5f99807a5bbe5dd39b377dd49f9c
Alexandra Simonova <mazukata@gmail.com>
2021-03-06 22:57:07 +0300

    refactor code

--- src/main/java/com/java/practice/task01/sorter/ArraySorter.java
+++ src/main/java/com/java/practice/task01/sorter/ArraySorter.java
@@ -3,15 +3,12 @@ package com.java.practice.task01.sorter;
 import java.util.Map;
 
 public class ArraySorter {
-    private static final Map<SortType, Sort> sorterMap;
-
-    static {
-        sorterMap = Map.of(SortType.BUBBLE_SORT, new BubbleSorter(),
-                SortType.INSERTION_SORT, new InsertionSorter(),
-                SortType.SELECTION_SORT, new SelectionSorter(),
-                SortType.MERGE_SORT, new MergeSorter(),
-                SortType.QUICK_SORT, new QuickSorter());
-    }
+    private static final Map<SortType, Sort> sorterMap =
+            Map.of(SortType.BUBBLE_SORT, new BubbleSorter(),
+                    SortType.INSERTION_SORT, new InsertionSorter(),
+                    SortType.SELECTION_SORT, new SelectionSorter(),
+                    SortType.MERGE_SORT, new MergeSorter(),
+                    SortType.QUICK_SORT, new QuickSorter());
 
     public void sort(int[] numbers, SortType sortType) {
         sorterMap.get(sortType).sort(numbers);
